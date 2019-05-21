cbbdfbb6b24dcd80cc7d600b2cafc35b1191b215
Tanner Gabriel <gabrieltanner.code@gmail.com>
2019-05-21 20:23:20 +0200

    Centered canvas

--- public/sketch.js
+++ public/sketch.js
@@ -1,64 +1,82 @@
 let socket
 let color = '#FFF'
 let strokeWidth = 4
+let cv
 
 function setup() {
-    const cv = createCanvas(800, 600)
-    cv.position(600, 100)
-    cv.background(0)
+	// Creating canvas
+	cv = createCanvas(windowWidth / 2, windowHeight / 2)
+	centerCanvas()
+	cv.background(0)
 
-    // Start the socket connection
-    socket = io.connect('http://localhost:3000')
+	// Start the socket connection
+	socket = io.connect('http://localhost:3000')
 
-    // Callback function
-    socket.on('mouse', data => {
-        stroke(data.color)
-        strokeWeight(data.strokeWidth)
-        line(data.x, data.y, data.px, data.py)
-    })
+	// Callback function
+	socket.on('mouse', data => {
+		stroke(data.color)
+		strokeWeight(data.strokeWidth)
+		line(data.x, data.y, data.px, data.py)
+	})
 
-    const color_picker = select('#pickcolor')
-    const color_btn = select('#color-btn')
-    const color_holder = select('#color-holder')
+	// Getting our buttons and the holder through the p5.js dom
+	const color_picker = select('#pickcolor')
+	const color_btn = select('#color-btn')
+	const color_holder = select('#color-holder')
 
-    const stroke_width_picker = select('#stroke-width-picker')
-    const stroke_btn = select('#stroke-btn')
+	const stroke_width_picker = select('#stroke-width-picker')
+	const stroke_btn = select('#stroke-btn')
 
-    color_btn.mousePressed(event => {
-        if (/(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(color_picker.value())) {
-            color = color_picker.value()
-            color_holder.style('background-color', color)
-        } else console.log('Enter a valid hex value')
-    })
+	// Adding a mousePressed listener to the button
+	color_btn.mousePressed(() => {
+		// Checking if the input is a valid hex color
+		if (/(^#[0-9A-F]{6}$)|(^#[0-9A-F]{3}$)/i.test(color_picker.value())) {
+			color = color_picker.value()
+			color_holder.style('background-color', color)
+		}
+		else {console.log('Enter a valid hex value')}
+	})
 
-    stroke_btn.mousePressed(event => {
-        const width = parseInt(stroke_width_picker.value())
-        if (width > 0) strokeWidth = width
-    })
+	// Adding a mousePressed listener to the button
+	stroke_btn.mousePressed(() => {
+		const width = parseInt(stroke_width_picker.value())
+		if (width > 0) strokeWidth = width
+	})
 }
 
-function draw() {}
+function windowResized() {
+	centerCanvas()
+	cv.resizeCanvas(windowWidth / 2, windowHeight / 2, false)
+}
+
+
+function centerCanvas() {
+	const x = (windowWidth - width) / 2
+	const y = (windowHeight - height) / 2
+	cv.position(x, y)
+}
+
 
 function mouseDragged() {
-    // Draw
-    stroke(color)
-    strokeWeight(strokeWidth);
-    line(mouseX, mouseY, pmouseX, pmouseY)
+	// Draw
+	stroke(color)
+	strokeWeight(strokeWidth)
+	line(mouseX, mouseY, pmouseX, pmouseY)
 
-    // Send the mouse coordinates
-    sendmouse(mouseX, mouseY, pmouseX, pmouseY)
+	// Send the mouse coordinates
+	sendmouse(mouseX, mouseY, pmouseX, pmouseY)
 }
 
 // Sending data to the socket
 function sendmouse(x, y, pX, pY) {
-    const data = {
-        x: x,
-        y: y,
-        px: pX,
-        py: pY,
-        color: color,
-        strokeWidth: strokeWidth
-    }
+	const data = {
+		x: x,
+		y: y,
+		px: pX,
+		py: pY,
+		color: color,
+		strokeWidth: strokeWidth,
+	}
 
-    socket.emit('mouse', data)
+	socket.emit('mouse', data)
 }
--- server.js
+++ server.js
@@ -2,58 +2,58 @@ const http = require('http')
 const app = require('./app')
 
 const normalizePort = val => {
-    const port = parseInt(val, 10)
+	const port = parseInt(val, 10)
 
-    if (isNaN(port)) {
-        return val
-    }
-    if (port >= 0) {
-        return port
-    }
-    return false
+	if (isNaN(port)) {
+		return val
+	}
+	if (port >= 0) {
+		return port
+	}
+	return false
 }
 const port = normalizePort(process.env.PORT || '3000')
 app.set('port', port)
 
 const errorHandler = error => {
-    if (error.syscall !== 'listen') {
-        throw error
-    }
-    const address = server.address()
-    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elevated privileges.')
-            process.exit(1)
-            break
-        case 'EADDRINUSE':
-            console.error(bind + ' is already in use.')
-            process.exit(1)
-            break
-        default:
-            throw error
-    }
+	if (error.syscall !== 'listen') {
+		throw error
+	}
+	const address = server.address()
+	const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port
+	switch (error.code) {
+	case 'EACCES':
+		console.error(bind + ' requires elevated privileges.')
+		process.exit(1)
+		break
+	case 'EADDRINUSE':
+		console.error(bind + ' is already in use.')
+		process.exit(1)
+		break
+	default:
+		throw error
+	}
 }
 
 const server = http.createServer(app)
 
 server.on('error', errorHandler)
 server.on('listening', () => {
-    const address = server.address()
-    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port
-    console.log('Listening on ' + bind)
+	const address = server.address()
+	const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port
+	console.log('Listening on ' + bind)
 })
 
 
 // Web sockets
-var io = require('socket.io')(server);
+const io = require('socket.io')(server)
 
 io.sockets.on('connection', (socket) => {
-    console.log("Client connected: " + socket.id);
+	console.log('Client connected: ' + socket.id)
 
-    socket.on('mouse', (data) => socket.broadcast.emit('mouse', data));
+	socket.on('mouse', (data) => socket.broadcast.emit('mouse', data))
 
-    socket.on('disconnect', () => console.log("Client has disconnected"));
-});
+	socket.on('disconnect', () => console.log('Client has disconnected'))
+})
 
 server.listen(port)
